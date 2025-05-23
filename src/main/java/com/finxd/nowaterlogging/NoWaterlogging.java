646467fbcd17834ce6b8d755ce8d41c2446d6849
Finxd <timphaapsaari@gmail.com>
2025-05-23 16:44:49 +0300

    initial commit

---
+++ src/main/java/com/finxd/nowaterlogging/NoWaterlogging.java
@@ -0,0 +1,42 @@
+package com.finxd.nowaterlogging;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(NoWaterlogging.MODID)
+public class NoWaterlogging
+{
+    public static final String MODID = "nowaterlogging";
+
+    public NoWaterlogging()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
---
+++ src/main/java/com/finxd/nowaterlogging/mixin/ScaffoldingBlockMixin.java
@@ -0,0 +1,41 @@
+package com.finxd.nowaterlogging.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.ScaffoldingBlock;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ScaffoldingBlock.class)
+
+public class ScaffoldingBlockMixin implements SimpleWaterloggedBlock {
+
+    //this removes the water from the block, doesn't remove the waterlogged property
+    //the block will have it's waterlogged property set to true when placing it in water, but it still looks and behaves like it never had water
+    @Inject(method = "getFluidState", at = @At("HEAD"), cancellable = true)
+    public void getFluidState(BlockState pState, CallbackInfoReturnable<FluidState> cir) {
+        cir.setReturnValue(Fluids.EMPTY.defaultFluidState());
+    }
+
+    //buckets no longer try to fill the block
+    @Override
+    public boolean canPlaceLiquid(BlockGetter pLevel, BlockPos pPos, BlockState pState, Fluid pFluid) {
+        return false;
+    }
+
+    //buckets no longer try to empty the block, if the block happens to have waterlogged set to true
+    @Override
+    public @NotNull ItemStack pickupBlock(LevelAccessor pLevel, BlockPos pPos, BlockState pState) {
+        return ItemStack.EMPTY;
+    }
+}
